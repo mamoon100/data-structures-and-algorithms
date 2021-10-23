297dc3cad40f7fc76c7d8dd0e2d28f61007c95c4
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-10-23 15:17:22 +0300

    finshed stack and queue with all the test and readme

---
+++ java/linked-list/app/src/main/java/linked/list/Queue.java
@@ -0,0 +1,61 @@
+package linked.list;
+
+public class Queue<T> {
+  Node<T> front;
+  Node<T> back;
+  int length = 0;
+
+  public Queue() {
+    this.front = null;
+    this.back = null;
+  }
+
+  public void enqueue(T value) {
+    Node<T> node = new Node<>(value);
+    if (isEmpty()) this.front = node;
+    else this.back.nextPointer = node;
+    this.back = node;
+    length++;
+  }
+
+  public T dequeue() {
+    Node<T> temp = this.front;
+    try {
+      if (isEmpty()) throw new NullPointerException("The Queue is empty nothing to dequeue \uD83E\uDD28");
+      this.front = this.front.nextPointer;
+      this.length--;
+      return temp.value;
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public T peek() {
+    try {
+      if (isEmpty()) throw new NullPointerException("The Queue is empty there no one in the line to peek \uD83E\uDD73");
+      return this.front.value;
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+
+  public boolean isEmpty() {
+    return this.front == null;
+  }
+
+  @Override
+  public String toString() {
+    Node<T> front = this.front;
+    String msg = "";
+    msg += "Front => ";
+    while (front != null) {
+      msg += "{" + front.value + "} =>";
+      front = front.nextPointer;
+    }
+    msg += " Null";
+    return msg;
+  }
+}
